d0563e7f235f29f4bc2f9d09a6ab283d93f396ae
Std1o <37378410+Std1o@users.noreply.github.com>
2021-02-27 00:12:18 +0300

    main logic

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstNum:Double = 0;
+    var secondNum:Double = 0;
+    var mathSign:Bool = false;
+    var operationTag:Int = -1;
     @IBOutlet weak var result: UILabel!
+    
     @IBAction func printDigit(_ sender: UIButton) {
+        if (result.text == "+" || result.text == "-" ||
+            result.text == "x" || result.text == "/") {
+            result.text = ""
+        }
         result.text! += String(sender.tag)
+        if !mathSign {
+            firstNum = Double(result.text!)!
+        } else {
+            secondNum = Double(result.text!)!
+        }
     }
     
+    @IBAction func clear(_ sender: UIButton) {
+        result.text = ""
+        firstNum = 0;
+        secondNum = 0;
+        mathSign = false;
+        operationTag = -1
+    }
+    
+    @IBAction func doOperation(_ sender: UIButton) {
+        if sender.tag != 4 {
+            operationTag = sender.tag
+        }
+        if !mathSign {
+            switch sender.tag {
+            case 0:
+                result.text! = "+"
+            case 1:
+                result.text! = "-"
+            case 2:
+                result.text! = "x"
+            case 3:
+                result.text! = "/"
+            default: break
+            }
+            mathSign = true
+        } else {
+            mathSign = false
+            switch operationTag {
+            case 0:
+                firstNum = firstNum + secondNum
+                result.text! = String(firstNum)
+            case 1:
+                firstNum = firstNum - secondNum
+                result.text! = String(firstNum)
+            case 2:
+                firstNum = firstNum * secondNum
+                result.text! = String(firstNum)
+            case 3:
+                firstNum = firstNum / secondNum
+                result.text! = String(firstNum)
+            default: break
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
